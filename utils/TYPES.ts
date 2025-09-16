a794f876785d19d7d16970ba073da78c9841664b
mazam5 <mohammedazam651@gmail.com>
2025-09-16 17:51:54 +0530

    added: types

---
+++ utils/TYPES.ts
@@ -0,0 +1,18 @@
+export interface API_DATA_1 {
+  cause_and_effect: {
+    cause: string;
+    effect: string;
+  };
+  citation: {
+    label: string;
+    url: string;
+  };
+  content: string;
+  image_url: string;
+  title: string;
+}
+export interface TYPE_FLASHCARD {
+  content: string;
+  image_url: string;
+  title: string;
+}
